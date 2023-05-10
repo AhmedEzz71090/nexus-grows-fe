a40622b048c0c417f18e8368f303e0b2620942f0
AhmedEzz71090 <a.ezz@afaqy.com>
2023-05-10 23:31:34 +0300

    Install fontawesome, restyle navbar, and add user settings menu.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 
 
@@ -12,7 +15,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    FontAwesomeModule
   ]
 })
 export class SharedModule { }
