c516a927d98388eec8d116dd0a187767e3b70da0
AhmedEzz71090 <a.ezz@afaqy.com>
2023-05-10 11:19:55 +0300

    Create products component

---
+++ src/app/products/components/products/products.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent {
+products = [
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+  {name: 'Products 1', description: 'lorem ipsum dolor sit amet, consectetur adip', image: 'https://picsum.photos/286/180'},
+]
+}
