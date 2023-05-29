222e5ae40ebcd9b8795122378061369aa56a56ee
AhmedEzz71090 <a.ezz@afaqy.com>
2023-05-29 13:34:34 +0300

    Create users component, and users table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from "./products/components/products/products.component";
 import {LandingComponent} from "./landing/components/landing/landing.component";
+import {UsersComponent} from "./users/users/users.component";
 
 const routes: Routes = [
   {path: '', component: LandingComponent},
-  {path: 'products', component: ProductsComponent}
+  {path: 'products', component: ProductsComponent},
+  {path: 'users', component: UsersComponent}
 ];
 
 @NgModule({
